ab9e6552b39e53e877524909a00893ed0a162dfd
ReymundoB <reymundo.b.z@gmail.com>
2023-07-02 21:51:59 -0600

    update-requirements and urls

--- final/urls.py
+++ final/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView, Spect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('personal/', include('personal.urls')),
+    path('', include('personal.urls')),
     path('api/', include('api.urls')),
     path('api/schema',SpectacularAPIView.as_view(),name="schema" ),
     path('api/schema/swagger-ui/',SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui' ),
